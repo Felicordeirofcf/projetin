9a6eec0b67670afd1d4225d5f8c4599a931f411a
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-04-17 09:42:18 -0300

    Mensagem explicando o que foi alterado

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,10 @@
-// src/api.js
 import axios from "axios";
 
-// Instância personalizada do Axios com baseURL da API
+// Pega da variável de ambiente REACT_APP_API_URL
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-// Interceptor para incluir token de autenticação (se existir no localStorage)
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -16,7 +14,6 @@ api.interceptors.request.use(
     return config;
   },
   (error) => {
-    // Trata erro ao preparar a requisição
     return Promise.reject(error);
   }
 );
