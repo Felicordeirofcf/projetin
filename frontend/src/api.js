bdda29f348472420e47e23a4621a055268ca647b
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-04-17 10:03:08 -0300

    Mensagem explicando o que foi alterado

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+// 👉 Adicionei o console.log aqui para debug
+console.log("API URL usada:", process.env.REACT_APP_API_URL);
+
+// Criação da instância do Axios com a URL do backend vinda da variável de ambiente
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // pegar do ambiente
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
+// Interceptador de requisições para incluir token JWT se existir
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
